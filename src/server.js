49245612734f7e325dba97e4920a7bdcff3b79d8
elinoza <hillcakmak@gmail.com>
2021-04-09 07:28:58 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,19 +25,19 @@ server.use(express.json());
 server.use(cookieParser())
 server.use(passport.initialize())
 
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
+// const whitelist = ["http://localhost:3000"]
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
 
-server.use(cors(corsOptions));
+server.use(cors());
 server.use("/users", userRoutes);
 server.use("/videos", videoRoutes);
 
